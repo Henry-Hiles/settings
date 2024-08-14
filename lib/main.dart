669f413d53c30e996652115d046d2994713a18db
Henry-Hiles <henry@henryhiles.com>
2024-08-14 16:05:22 -0400

    Broken

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:settings/widgets/app.dart';
 import 'package:window_manager/window_manager.dart';
@@ -5,10 +7,13 @@ import 'package:window_manager/window_manager.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await windowManager.ensureInitialized();
-  await windowManager.waitUntilReadyToShow(const WindowOptions(
-    backgroundColor: Colors.transparent,
-    titleBarStyle: TitleBarStyle.hidden,
-  ));
+  await windowManager.waitUntilReadyToShow(
+    const WindowOptions(
+      backgroundColor: Colors.transparent,
+      titleBarStyle: TitleBarStyle.hidden,
+    ),
+    () => windowManager.show(),
+  );
 
   runApp(const App());
 }
